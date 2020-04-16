aee48e400278bf1fb16cd7430daeb3caa4c22d7d
amin havasi <pau.ahq@gmail.com>
2020-04-16 20:22:05 +0430

    sec

--- app.js
+++ app.js
@@ -1,7 +1,45 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphQlHTTp = require('express-graphql');
+const { buildSchema } = require('graphql');
 const app = express();
+app.use(bodyParser.json());
+
+app.use(
+    '/graphql',
+    graphQlHTTp({
+        schema: buildSchema(`
+
+        type RootQuery{
+            events:[String!]!
+
+        }
+
+        type RootMutation{
+            createEvent(name:String):String
+
+        }
+
+        schema{
+            query:RootQuery
+
+
+            mutation:RootMutation
+        }
+        `),
+        rootValue: {
+            events: () => {
+                return ['ssd', 'All-Night Coding'];
+            },
+            createEvent: (args) => {
+                const eventName = args.name;
+                return eventName;
+            },
+        },
+        graphiql: true,
+    })
+);
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
